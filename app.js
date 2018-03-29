b9b1a2458170ebaff848356b7e8c8bafc8dafd6e
Jacob Flores <jacob.e.flores562@gmail.com>
2018-03-28 18:31:59 -0700

    doIt needed

--- app.js
+++ app.js
@@ -23,15 +23,21 @@ var client_Spotify = new Spotify({
 
 var  omdb_key = process.env.OMDB_API_KEY
 
+//passing in artist names for mapping
+function names(artist){
+    return artist.name
+}
 
-var params = {screen_name: 'jcbthnflrs', count: 5}
+var params = {screen_name: 'jcbthnflrs', count: 20}
 
 function callTwitter(){
     client_Twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
+            var tweetArray = []
           for(i=0; i<tweets.length; i++){
-              console.log(tweets[i].text)
+              tweetArray.push(tweets[i].text)
           }
+          console.log(tweetArray)
         }
         else{
             console.log(error)
@@ -41,9 +47,13 @@ function callTwitter(){
 }
 
 function callSpotify(){
-    client_Spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    var song = process.argv.slice(3)
+    client_Spotify.search({ type: 'track', query: song }, function(err, data) {
         if ( !err ) {
-            console.log("spotify: " + data.tracks.items[0].name)
+            console.log("Artist: " + data.tracks.items[0].artists.map(names))
+            console.log("Song: " + data.tracks.items[0].name)
+            console.log("Link: " + data.tracks.items[0].preview_url)
+            console.log("Album: " + data.tracks.items[0].album.name)
         }
         else{
             console.log('Error occurred: ' + err)
@@ -54,8 +64,9 @@ function callSpotify(){
 }
 
 function omdbCall() {
-    // var title = process.argv[3]
-    var title = 'avatar'
+    var title = process.argv.slice(3)
+    
+
     var omdbURL = "http://www.omdbapi.com/?apikey="+ omdb_key +"&t=" + title
 
    request(omdbURL, function(err, res, body){
@@ -63,12 +74,36 @@ function omdbCall() {
            console.log(err)
        }
        else{
-           console.log("body: " + body)
+           
+           var movie = JSON.parse(body)
+           console.log("Title: " + movie.Title)
+           console.log("Year: " + movie.Year)
+           console.log("IMDB: " + movie.Ratings[0].Value)
+           console.log("Rotten Tomatoes: " + movie.Ratings[1].Value)
+           console.log("Country: " + movie.Country)
+           console.log("Language: " + movie.Language)
+           console.log("Plot: " + movie.Plot)
+           console.log("Actors: " + movie.Actors)
        }
    })
 }
 
+var command = process.argv[2]
 
+switch(command){
+    case 'movie-this':
+        omdbCall()
+        break
+    case 'my-tweets':
+        callTwitter()
+        break
+    case 'spotify-this-song':
+        callSpotify()
+        break
+    case 'write-to-file':
+        doIt()
+        break
+    default:
+        console.log("the only commands are movie-this <movie>, my-tweets, spotify-this-song <song>")
+}
 
-
-omdbCall()
